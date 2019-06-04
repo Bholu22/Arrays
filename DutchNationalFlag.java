d72a196e3a7fd9a7f300af0a7fa0b35df12cf0e1
Bholu22 <43365520+Bholu22@users.noreply.github.com>
2019-06-04 06:50:44 +0530

    Update DutchNationalFlag.java

--- DutchNationalFlag.java
+++ DutchNationalFlag.java
@@ -8,17 +8,17 @@ public class DutchNationalFlag{
 		while(mid<=high){
 			switch(arr[mid])
 			{
-				case 0:  temp=arr[low];
+				case 0:          temp=arr[low];
 						 arr[low]=arr[mid];
 						 arr[mid]=temp;
 						 low++;
 						 mid++;
 						 break;
 				
-				case 1:  mid++;
-					     break;
+				case 1:          mid++;
+					         break;
 						 
-				case 2:  temp=arr[mid];
+				case 2:          temp=arr[mid];
 						 arr[mid]=arr[high];
 						 arr[high]=temp;
 						 high--;
